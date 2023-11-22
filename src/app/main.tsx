7e4814ce5ae5df90a9d3ae8b47964bc2e56d2e93
Smuggs <102694816+Smuggler911@users.noreply.github.com>
2023-11-22 17:15:30 +0400

    feat:add board creation

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import '../assets/styles/index.scss'
-import {RouterProvider} from "react-router-dom";
-import {appRoutes} from "./appRoutes";
-import SideBar from "../bars/sideBar";
-import SearchBar from "../bars/searchBar";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../assets/styles/index.scss";
+import { RouterProvider } from "react-router-dom";
+import { appRoutes } from "./appRoutes";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <RouterProvider router={appRoutes}/>
-    </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={appRoutes} />
+  </React.StrictMode>,
+);
--- src/pages/Board/interface/child/Kanban.tsx
+++ src/pages/Board/interface/child/Kanban.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Kanban.scss";
 import KanbanBoards from "../store";
 import { observer } from "mobx-react";
+import plusButton from "../assets/plus-large-svgrepo-com.svg";
+import exitButton from "../assets/cross-svgrepo-com.svg";
 
 export const Kanban = observer(() => {
   const { getKanbanBoards } = KanbanBoards;
@@ -9,16 +11,37 @@ export const Kanban = observer(() => {
     await getKanbanBoards();
   });
   const kanbanData = JSON.parse(localStorage.getItem("currentBoard") || "");
+  const createToDoForm = useRef<HTMLDivElement>(null);
+  const boardContainer = useRef<HTMLDivElement>(null);
+  const ExitForm = () => {
+    if (createToDoForm.current !== null && boardContainer.current !== null) {
+      createToDoForm.current.style.display = "none";
+      boardContainer.current.style.opacity = "100%";
+      boardContainer.current.style.transition = "all .3s ";
+    }
+  };
+  const EnterForm = () => {
+    if (createToDoForm.current !== null && boardContainer.current !== null) {
+      createToDoForm.current.style.display = "flex";
+      boardContainer.current.style.opacity = "20%";
+      boardContainer.current.style.transition = "all .3s ";
+    }
+  };
   return (
     <>
       <div className={"kanbanBoard-header"}>
         <div className={"kanban-name"}>
           <h1>{kanbanData.name}</h1>
         </div>
-        <div className={"kanbanBoard"}>
+        <div className={"kanbanBoard"} ref={boardContainer}>
           <div className={"toDo"}>
             <div className={"toDo-header"}>
               <h1>To Do</h1>
+              <div className={"header-button"}>
+                <button onClick={EnterForm}>
+                  <img src={plusButton} />
+                </button>
+              </div>
             </div>
           </div>
           <div className={"inProgress"}>
@@ -32,6 +55,27 @@ export const Kanban = observer(() => {
             </div>
           </div>
         </div>
+      </div>
+      <div className={"createToDo"} ref={createToDoForm}>
+        <form>
+          <div className={"exit-ToDoCreation"}>
+            <span onClick={ExitForm}>
+              <img src={exitButton} />
+            </span>
+          </div>
+          <div className={"toDoCreation-header"}>
+            <h1>Create new Task</h1>
+          </div>
+          <div className={"toDoCreation-taskInput"}>
+            <textarea
+              placeholder={"create new task"}
+              name={"taskInput"}
+            ></textarea>
+          </div>
+          <div className={"toDoCreation-submit"}>
+            <button type={"submit"}>Create</button>
+          </div>
+        </form>
       </div>
     </>
   );
